39b0d29daba0e8e8e4d9f0114758865c8016f01f
nn389 <nn389@drexel.edu>
2019-05-02 14:06:31 -0400

    second time - add location, markers, leaflets

--- search_bar.js
+++ search_bar.js
@@ -112,7 +112,8 @@ var m4 = {location: [43.2632857, -79.9184003], property: {elevation: 0, title: "
 var m5 = {location: [43.2635047, -79.9190461], property: {elevation: 0, title: "University hall - McMaster University"}}
 var m6 = {location: [43.2595923, -79.9175896], property: {elevation: 0, title: "McMaster Health Sciences Center - McMaster University"}}
 var m7 = {location: [43.2620513, -79.9201806], property: {elevation: 0, title: "Burke Science Building - McMaster University"}}
-
+var m8 = {location: [43.2627625, -79.9176234], property: {elevation: 0, title: "Mills Memorial Library - McMaster University"}}
+var m9 = {location: [43.263207, -79.9201646], property: {elevation: 0, title: "Edwards Hall - McMaster University"}}
 
 var marker1 = L.marker(m1.location, m1.property).addTo(map);
 var marker2 = L.marker(m2.location, m2.property).addTo(map);
@@ -120,7 +121,9 @@ var marker3 = L.marker(m3.location, m3.property).addTo(map);
 var marker4 = L.marker(m4.location, m4.property).addTo(map);
 var marker5 = L.marker(m5.location, m5.property).addTo(map);
 var marker6 = L.marker(m6.location, m6.property).addTo(map);
-var marker7 = L.marker(m6.location, m6.property).addTo(map);
+var marker7 = L.marker(m7.location, m7.property).addTo(map);
+var marker8 = L.marker(m8.location, m8.property).addTo(map);
+var marker9 = L.marker(m9.location, m9.property).addTo(map);
 
 marker1.bindPopup(m1.property.title).openPopup();
 marker2.bindPopup(m2.property.title).openPopup();
@@ -128,6 +131,9 @@ marker3.bindPopup(m3.property.title).openPopup();
 marker4.bindPopup(m4.property.title).openPopup();
 marker5.bindPopup(m5.property.title).openPopup();
 marker6.bindPopup(m6.property.title).openPopup();
-marker7.bindPopup(m6.property.title).openPopup();
+marker7.bindPopup(m7.property.title).openPopup();
+marker8.bindPopup(m8.property.title).openPopup();
+marker9.bindPopup(m9.property.title).openPopup();
+
 //end adding markers and leaflets
 //
