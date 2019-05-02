f03f1221d15f0e56dfef9fb0e5a91b2c26ec6b20
nn389 <nn389@drexel.edu>
2019-05-02 13:59:57 -0400

    add location, markers, leaflets

--- search_bar.js
+++ search_bar.js
@@ -10,7 +10,9 @@ var locations = [
     {title: "Drexel University (Disque Hall)", subtitle:"Philadelphia, PA", location: {latLng: L.latLng(39.9545366, -75.1877259), zoom: 18, headingDegrees: 0}},
     {title: "Drexel University (Stratton Hall)", subtitle:"Philadelphia, PA", location: {latLng: L.latLng(39.9541605, -75.18797), zoom: 18, headingDegrees: 0}},
     {title: "McMaster University", subtitle:"Ontario, Canada", location: {latLng: L.latLng(43.2632857, -79.9184003), zoom: 15, headingDegrees: -30}},
-    {title: "University Hall", subtitle:"MacMaster University", location: {latLng: L.latLng(43.2635047, -79.9190461), zoom: 20, headingDegrees: 0}},
+    {title: "University Hall", subtitle:"McMaster University", location: {latLng: L.latLng(43.2635047, -79.9190461), zoom: 20, headingDegrees: 0}},
+    {title: "McMaster Health Sciences Center", subtitle:"McMaster University", location: {latLng: L.latLng(43.2595923, -79.9175896), zoom: 15, headingDegrees: -30}},
+    {title: "Burke Science Building", subtitle:"McMaster University", location: {latLng: L.latLng(43.2620513, -79.9201806), zoom: 20, headingDegrees: 0}}
 ];
 
 var calculateDistSqr = function(latLng1, latLng2) {
@@ -70,7 +72,9 @@ var searchbarConfig = {
         {name: "Drexel University (Main Building)", zoom: 20, latLng: [39.9541718, -75.186816]},
         {name: "Drexel University (Disque Hall)", zoom: 18, latLng: [39.9545366, -75.1877259]},
         {name: "Drexel University (Stratton Hall)", latLng: [39.9541605, -75.18797]},
-        {name: "University Hall", zoom: 20, latLng: [43.2635047, -79.9190461]}
+        {name: "University Hall", zoom: 20, latLng: [43.2635047, -79.9190461]},
+        {name: "McMaster Health Sciences Center)", latLng: [43.2595923, -79.9175896]},
+        {name: "Burke Science Building", zoom: 20, latLng: [43.2620513, -79.9201806]}
     ]
 
 };
@@ -103,10 +107,12 @@ function openSelectedResultPopup(event) {
 //start adding markers and leaflets
 var m1 = {location: [39.9541718, -75.186816], property: {elevation: 0, title: "Main Building - Drexel University"}}
 var m2 = {location: [39.9545366, -75.1877259], property: {elevation: 0, title: "Disque hall - Drexel University"}}
-var m3 = {location: [39.9541605, -75.18797], property: {elevation: 0, title: "Straton hall - Drexel University"}}
+var m3 = {location: [39.9541605, -75.18797], property: {elevation: 0, title: "Stratton hall - Drexel University"}}
 var m4 = {location: [43.2632857, -79.9184003], property: {elevation: 0, title: "MacMaster University"}}
 var m5 = {location: [43.2635047, -79.9190461], property: {elevation: 0, title: "University hall - McMaster University"}}
-var m6 = {location: [39.9545366, -75.1877259], property: {elevation: 0, title: "Drexel University"}}
+var m6 = {location: [43.2595923, -79.9175896], property: {elevation: 0, title: "McMaster Health Sciences Center - McMaster University"}}
+var m7 = {location: [43.2620513, -79.9201806], property: {elevation: 0, title: "Burke Science Building - McMaster University"}}
+
 
 var marker1 = L.marker(m1.location, m1.property).addTo(map);
 var marker2 = L.marker(m2.location, m2.property).addTo(map);
@@ -114,6 +120,7 @@ var marker3 = L.marker(m3.location, m3.property).addTo(map);
 var marker4 = L.marker(m4.location, m4.property).addTo(map);
 var marker5 = L.marker(m5.location, m5.property).addTo(map);
 var marker6 = L.marker(m6.location, m6.property).addTo(map);
+var marker7 = L.marker(m6.location, m6.property).addTo(map);
 
 marker1.bindPopup(m1.property.title).openPopup();
 marker2.bindPopup(m2.property.title).openPopup();
@@ -121,5 +128,6 @@ marker3.bindPopup(m3.property.title).openPopup();
 marker4.bindPopup(m4.property.title).openPopup();
 marker5.bindPopup(m5.property.title).openPopup();
 marker6.bindPopup(m6.property.title).openPopup();
+marker7.bindPopup(m6.property.title).openPopup();
 //end adding markers and leaflets
 //
